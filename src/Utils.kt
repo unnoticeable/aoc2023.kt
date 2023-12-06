81c33e6888be91bb7b78b0f50954530e2963e1b9
unnoticeable <60264888+unnoticeable@users.noreply.github.com>
2023-12-06 02:16:48 -0500

    Day 5

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,3 +5,4 @@ fun readInput(name: String) = Path("src/${name}.txt").readText().trim()
 fun intOf(s: String) = Regex("""\d+""").find(s)!!.value.toInt()
 fun intsOf(s: String) = Regex("""\d+""").findAll(s).map { it.value.toInt() }
 fun wordOf(s: String) = Regex("""\p{Alpha}+""").find(s)!!.value
+fun longsOf(s: String) = Regex("""\d+""").findAll(s).map { it.value.toLong() }
